8cf2ed73aa23e4820e78a64edb80f4e0ccd61afe
Kostya Kononov <kononov@a2design.biz>
2022-01-26 19:31:02 +0600

    Added log out route.

---
+++ src/routes/log-out.svelte
@@ -0,0 +1,15 @@
+<script lang="ts" context="module">
+	import { dbUser } from 'api/user';
+	export function load() {
+		dbUser.leave(undefined);
+
+		return {
+			redirect: '/sign-in',
+			status: 302
+		};
+	}
+</script>
+
+<svelte:head>
+	<title>Log out | SPEAK.io</title>
+</svelte:head>
